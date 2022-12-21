0bc43e9a47a6351ca81c4be4299f39a578dceea8
Ashutosh Mahapatra <ashutoshm439@gmail.com>
2022-12-21 21:29:50 +0530

    new next

--- pages/details/[id].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function id(){
-    return (
-        <h1>id</h1>
-    )
-}
--- pages/details/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Details(){
-    return(
-        <h1>details</h1>
-    )
-}
